9cf5203e9ed04ff4f7cf4ca0eb7fbaa549831333
omkarSw2 <omkar21143@gmail.com>
2024-08-08 16:43:58 +0530

    made backend

--- backend/src/app.ts
+++
@@ -1,9 +0,0 @@
-import express from "express";
-const app = express();
-app.use(express.json());
-
-app.get("/", (req, res, next) => {
-  res.status(200).send("Hello World omkar");
-});
-
-export default app;
